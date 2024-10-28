6850a014821b6763abcf45aa9b363d5ce8c388c4
Megha N <n.megha82@gmail.com>
2024-10-27 18:43:46 -0700

    Initial code for frontend

---
+++ ForumPOCFrontEnd/Forum/forum.js
@@ -0,0 +1,13 @@
+// script.js
+const newPostButton = document.querySelector('.new-post');
+const searchIcon = document.querySelector('.search-icon');
+
+newPostButton.addEventListener('click', () => {
+  // Add your logic for creating a new post here
+  alert('New post button clicked!');
+});
+
+searchIcon.addEventListener('click', () => {
+  // Add your logic for searching posts here
+  alert('Search button clicked!');
+});
